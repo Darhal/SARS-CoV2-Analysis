c777cc203f8ad278d8244217d0845996bc3d9570
Chaima TOUNSI OMEZZINE <chaima.tounsi-omezzine@telecomnancy.eu>
2020-11-12 18:50:50 +0100

    le nombre des nucleotides

--- src/utility.py
+++ src/utility.py
@@ -7,3 +7,19 @@ def FastaToGenome(filename):
         genome.append(seq_record.seq)
         
     return genome
+
+def total_nucleotide(ARNm):         #nombre total des nucléotides dans une sequence
+    return len(ARNm)
+
+def nombre_nucleotide(ARNm):        #retourne un dictionnaire indiquant le nombre de chaque type de nucléotide
+    d={'A':0, 'T':0, 'G':0, 'C':0}
+    for i in range(len(ARNm)):
+        if ARNm[i]=='A':
+            d['A']+=1
+        elif ARNm[i]=='T':
+            d['T']+=1
+        elif ARNm[i]=='G':
+            d['G']+=1
+        elif ARNm[i] =='C':
+            d['C']+=1
+    return d
