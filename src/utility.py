f54ed279953c1479df2754098816809354805aff
Omar <chidaomar46@gmail.com>
2020-11-14 21:08:11 +0100

    Added comments to the functions.

--- src/utility.py
+++ src/utility.py
@@ -1,18 +1,45 @@
 from Bio import SeqIO
 
 def fasta_to_genome(filename):
+    '''Function that parse a fasta file
+    
+    Args:
+        filename: the fasta file that contain the genomic data
+
+    Returns:
+        The first sequence if the file contains only one, a table of sequences otherwise
+    '''
     genome = []
 
     for seq_record in SeqIO.parse("../genome/"+filename, "fasta"):
         genome.append(seq_record.seq)
 
+    if len(genome) == 1:
+        return genome[0]
     return genome
 
 
-def total_nucleotide(ARNm):         #nombre total des nucléotides dans une sequence
+def total_nucleotide(ARNm):
+    '''Nombre total des nucléotides dans une sequence
+
+    Args:
+        ARNm: the RNA sequence
+
+    Returns:
+        Length of the RNA sequence
+    '''
     return len(ARNm)
 
-def nombre_nucleotide(ARNm):        #retourne un dictionnaire indiquant le nombre de chaque type de nucléotide
+
+def nombre_nucleotide(ARNm):
+    '''Retourne un dictionnaire indiquant le nombre de chaque type de nucléotide
+
+    Args:
+        ARNm: the RNA sequence
+    
+    Returns:
+        Dictionary that contains the number of nucleotides as value and nucleotide as key
+    '''
     d = {'A':0, 'T':0, 'G':0, 'C':0}
 
     for i in range(len(ARNm)):
