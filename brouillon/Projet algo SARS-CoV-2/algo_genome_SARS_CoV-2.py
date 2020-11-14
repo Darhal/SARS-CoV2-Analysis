9d063d40b4cf4cb657d6e4aee1b04a4e4d905e3a
Céline Zhang <celine.zhang@telecomnancy.eu>
2020-11-14 20:25:26 +0100

    changement style

--- brouillon/Projet algo SARS-CoV-2/algo_genome_SARS_CoV-2.py
+++ brouillon/Projet algo SARS-CoV-2/algo_genome_SARS_CoV-2.py
@@ -9,6 +9,7 @@ import timeit
 import matplotlib
 import math
 
+
 #def sum(n):
 #    res = 0
 #    for k in range(n):
@@ -22,71 +23,91 @@ import math
 #    return S
 #Seq.read(open(fasta)),'fasta',alphabet=generic_dna).seq
 
-S =[]
-for seq in SeqIO.parse(open('sous-ensemble_1_nucleotide_ver.fasta'),'fasta'):
-    S.append(seq)
-print(S)
+
+ S =[]
+
+ for seq in SeqIO.parse(open('sous-ensemble_1_nucleotide_ver.fasta'),'fasta'):
+     S.append(seq)
+ print(S)
+
 
 def taille_ensemble(L): # taille d'une liste de séquence
     l = []
+
     for k in L:
         l.append(len(k))
     return l
 
+
 #def tri(L): # peut être écrit si .sort n'est pas donné
+
 
 def moyenne(list): # on prend en entée une liste de valeur
     if not list:
         return None
+    
     res = 0
+
     for k in list:
         res += k
     return res/len(list)
 
+
 def mediane(list): # on prend en entrée une liste de valeur
     if not list:
         return None
+
     L = list.sort()
     n = len(L)
+
     if n%2 == 0:
         return (L[n/2]+L[n/2-1])/2
     else:
         return L[(n-1)/2]
+
 
 def quartile(n,list): # quartile(2,list) est la médiane
     if not list:
         return None
     if n not in [1,2,3]:
         return None
+    
     L = list.sort()
     t = len(list)
+    
     if n == 1:
         if t%4 == 0:
             return (L[n/4]+L[n/4-1])/2
         else:
             return L[int(n/4)]
-    if n == 2:
+    elif n == 2:
         return mediane(list)
-    if n == 3:
+    elif n == 3:
         if t%4 == 0:
             return (L[3*n/4]+L[3*n/4-1])/2
         else:
             return L[int(3*n/4)]
 
+
 def taux_GC123_ensemble(L): # taux GC123 du'une liste de séquence
     l = []
+
     for seq in L:
         l.append(GC123(seq))
     return l
 
+
 def variance(list):
     if not list:
         return None
+
     res = 0
+
     m = moyenne(list)
     for k in list:
         res += (m-k)**2
     return res/len(list)
+
 
 def ecart_type(list):
     return math.sqrt(variance(list))
