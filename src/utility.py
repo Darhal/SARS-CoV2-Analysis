261707e80551456494281620aaf5c5da5b2c9638
Omar <chidaomar46@gmail.com>
2020-11-14 22:33:47 +0100

    resolved a conflict

--- src/utility.py
+++ src/utility.py
@@ -19,20 +19,6 @@ def fasta_to_genome(filename):
     return genome
 
 
-<<<<<<< HEAD
-def total_nucleotide(ARNm):
-    '''
-        Fonction qui retourne le nombre total des nucléotides dans une sequence
-
-        Args:
-            ARNm: Seq d'ARN
-
-        Returns:
-            un entier qui représente la taille de seq
-        '''
-||||||| 8f14109
-def total_nucleotide(ARNm):         #nombre total des nucléotides dans une sequence
-=======
 def total_nucleotide(ARNm):
     '''Nombre total des nucléotides dans une sequence
 
@@ -42,23 +28,8 @@ def total_nucleotide(ARNm):
     Returns:
         Length of the RNA sequence
     '''
->>>>>>> 18592bf7fccc9fc0e711b37d84cf48bfaf2c1468
     return len(ARNm)
 
-<<<<<<< HEAD
-def nombre_nucleotide(ARNm):
-    '''
-    Fonction qui retourne le nombre de chaque type de nucléotide
-
-    Args:
-        ARNm: Seq d'ARN
-
-    Returns:
-        un dictionnaire qui prend les nucléotides comme clés et le nombre de chaque nucléotides comme valeurs
-    '''
-||||||| 8f14109
-def nombre_nucleotide(ARNm):        #retourne un dictionnaire indiquant le nombre de chaque type de nucléotide
-=======
 
 def nombre_nucleotide(ARNm):
     '''Retourne un dictionnaire indiquant le nombre de chaque type de nucléotide
@@ -69,7 +40,6 @@ def nombre_nucleotide(ARNm):
     Returns:
         Dictionary that contains the number of nucleotides as value and nucleotide as key
     '''
->>>>>>> 18592bf7fccc9fc0e711b37d84cf48bfaf2c1468
     d = {'A':0, 'T':0, 'G':0, 'C':0}
 
     for i in range(len(ARNm)):
