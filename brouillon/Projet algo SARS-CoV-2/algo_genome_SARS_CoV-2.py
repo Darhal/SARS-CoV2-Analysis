66da4b8a33039a753fe1fd52eae237da8661e750
Céline Zhang <celine.zhang@telecomnancy.eu>
2020-11-14 21:24:18 +0100

    dépôt des fonctions sur le fichier stats.py

--- brouillon/Projet algo SARS-CoV-2/algo_genome_SARS_CoV-2.py
+++ brouillon/Projet algo SARS-CoV-2/algo_genome_SARS_CoV-2.py
@@ -24,14 +24,24 @@ import math
 #Seq.read(open(fasta)),'fasta',alphabet=generic_dna).seq
 
 
- S =[]
+#  S =[]
 
- for seq in SeqIO.parse(open('sous-ensemble_1_nucleotide_ver.fasta'),'fasta'):
-     S.append(seq)
- print(S)
+#  for seq in SeqIO.parse(open('sous-ensemble_1_nucleotide_ver.fasta'),'fasta'):
+#      S.append(seq)
+#  print(S)
 
 
-def taille_ensemble(L): # taille d'une liste de séquence
+# taille_ensemble([seqA, seqB, seqC]) = [len(seqA),len(seqB),len(seqC)]
+
+def taille_ensemble(L): # liste de la taille de chaque séquence d'une liste de séquence
+    ''' on aura un échantillon de séquence, la fonction permet de donner la taille de chaque séquence dans une liste
+
+    Args :
+        liste de séquences
+
+    Returns :
+        liste de la taille de chaque séquence de l'échantillon
+    '''
     l = []
 
     for k in L:
@@ -43,6 +53,14 @@ def taille_ensemble(L): # taille d'une liste de séquence
 
 
 def moyenne(list): # on prend en entée une liste de valeur
+    ''' la fonction fait la moyenne des valeurs d'une liste
+
+    Args :
+        liste de valeurs
+
+    Returns :
+        la moyenne des valeurs
+    '''
     if not list:
         return None
     
@@ -54,6 +72,14 @@ def moyenne(list): # on prend en entée une liste de valeur
 
 
 def mediane(list): # on prend en entrée une liste de valeur
+    ''' la fonction donne la médiane de l'échantillon
+
+    Args :
+        liste de valeurs
+
+    Returns :
+        la médiane de la liste
+    '''
     if not list:
         return None
 
@@ -67,6 +93,14 @@ def mediane(list): # on prend en entrée une liste de valeur
 
 
 def quartile(n,list): # quartile(2,list) est la médiane
+    ''' la fonction donne le quartile (1er, 2e, ou 3e) de l'échantillon
+
+    Args :
+        liste de valeurs
+
+    Returns :
+        le quartile (1er, 2e, ou 3e) de la liste
+    '''
     if not list:
         return None
     if n not in [1,2,3]:
@@ -75,21 +109,21 @@ def quartile(n,list): # quartile(2,list) est la médiane
     L = list.sort()
     t = len(list)
     
-    if n == 1:
+    if n == 1: # le 1er quartile
         if t%4 == 0:
             return (L[n/4]+L[n/4-1])/2
         else:
             return L[int(n/4)]
-    elif n == 2:
+    elif n == 2: # la médiane
         return mediane(list)
-    elif n == 3:
+    elif n == 3: # le 3e quartile
         if t%4 == 0:
             return (L[3*n/4]+L[3*n/4-1])/2
         else:
             return L[int(3*n/4)]
 
 
-def taux_GC123_ensemble(L): # taux GC123 du'une liste de séquence
+def taux_GC123_ensemble(L): # taux GC123 d'une liste de séquence, ça donne le taux de GC dans liste de séquences
     l = []
 
     for seq in L:
@@ -98,17 +132,33 @@ def taux_GC123_ensemble(L): # taux GC123 du'une liste de séquence
 
 
 def variance(list):
+    ''' la fonction calcule la variance de l'échantillon
+
+    Args :
+        liste de valeurs
+
+    Returns :
+        la variance de la liste
+    '''
     if not list:
         return None
 
     res = 0
-
     m = moyenne(list)
+
     for k in list:
         res += (m-k)**2
     return res/len(list)
 
 
 def ecart_type(list):
+    ''' la fonction calcule l'écart-type de l'échantillon
+
+    Args :
+        liste de valeurs
+
+    Returns :
+        l'écart-type' de la liste
+    '''
     return math.sqrt(variance(list))
 
