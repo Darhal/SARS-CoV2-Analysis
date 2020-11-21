99c6aae599309cc6a17b9f1d717ef84a4c9866a0
Chaima TOUNSI OMEZZINE <chaima.tounsi-omezzine@telecomnancy.eu>
2020-11-21 20:45:02 +0100

    modification de noms

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -9,10 +9,10 @@ def test_total_elements():
 
 
 def test_nombre_elements():
-    assert total_elements(['A', 'C', 'U']) == {'A': 1, 'U': 1, 'G': 0, 'C': 1}
-    assert total_elements(['A', 'A', 'A']) == {'A': 3, 'U': 0, 'G': 0, 'C': 0}
-    assert total_elements(['A', 'C', 'U']) == {'A': 1, 'U': 1, 'G': 0, 'C': 1}
-    assert total_elements(['A', 'C', 'U', 'G', 'A', 'C', 'U']) == {'A': 2, 'U': 2, 'G': 1, 'C': 2}
+    assert nombre_elements(['A', 'C', 'U']) == {'A': 1, 'U': 1, 'G': 0, 'C': 1}
+    assert nombre_elements(['A', 'A', 'A']) == {'A': 3, 'U': 0, 'G': 0, 'C': 0}
+    assert nombre_elements(['A', 'C', 'U']) == {'A': 1, 'U': 1, 'G': 0, 'C': 1}
+    assert nombre_elements(['A', 'C', 'U', 'G', 'A', 'C', 'U']) == {'A': 2, 'U': 2, 'G': 1, 'C': 2}
 
 
 
