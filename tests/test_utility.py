5295c8520baae0b4c5edfd2a044bf325682086e3
Chaima TOUNSI OMEZZINE <chaima.tounsi-omezzine@telecomnancy.eu>
2020-11-21 20:41:00 +0100

    modification de noms

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -2,13 +2,13 @@ import pytest
 
 from src.utility import *
 
-def test_total_nucleotide():
+def test_total_elements():
     assert total_elements(['A', 'C', 'U']) == 3
     assert total_elements(['A', 'C', 'U','C', 'U']) == 5
     assert total_elements([]) == 0
 
 
-def test_nombre_nucleotide():
+def test_nombre_elements():
     assert total_elements(['A', 'C', 'U']) == {'A': 1, 'U': 1, 'G': 0, 'C': 1}
     assert total_elements(['A', 'A', 'A']) == {'A': 3, 'U': 0, 'G': 0, 'C': 0}
     assert total_elements(['A', 'C', 'U']) == {'A': 1, 'U': 1, 'G': 0, 'C': 1}
