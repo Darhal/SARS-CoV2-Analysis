e7bac70969b26b41adf07397514e471cca414847
Chaima TOUNSI OMEZZINE <chaima.tounsi-omezzine@telecomnancy.eu>
2020-12-05 00:16:25 +0100

    test nombre elements

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -21,3 +21,23 @@ def test_nombre_elements():
     assert nombre_elements(['A', 'A', 'A'], NUCLEOTIDES) == {'A': 3, 'U': 0, 'G': 0, 'C': 0}
     assert nombre_elements(['A', 'C', 'U'], NUCLEOTIDES) == {'A': 1, 'U': 1, 'G': 0, 'C': 1}
     assert nombre_elements(['A', 'C', 'U', 'G', 'A', 'C', 'U'], NUCLEOTIDES) == {'A': 2, 'U': 2, 'G': 1, 'C': 2}
+
+    assert nombre_elements(['F', 'L', 'I', 'Q'], AMINO_ACIDS) == {'A': 0, 'R': 0, 'N': 0, 'D': 0, 'C': 0, 'Q': 1,
+                                                                  'E': 0, 'G': 0, 'H': 0, 'I': 1, 'L': 1, 'K': 0, 'M': 0,
+                                                                  'F': 1, 'P': 0, 'O': 0, 'S': 0, 'U': 0, 'T': 0, 'W': 0,
+                                                                  'Y': 0, 'V': 0, 'B': 0, 'Z': 0, 'X': 0, 'J': 0, '*': 0}
+
+    assert nombre_elements(['A', 'A', 'A'], AMINO_ACIDS) == {'A': 3, 'R': 0, 'N': 0, 'D': 0, 'C': 0, 'Q': 0,
+                                                              'E': 0, 'G': 0, 'H': 0, 'I': 0, 'L': 0, 'K': 0, 'M': 0,
+                                                              'F': 0, 'P': 0, 'O': 0, 'S': 0, 'U': 0, 'T': 0, 'W': 0,
+                                                              'Y': 0, 'V': 0, 'B': 0, 'Z': 0, 'X': 0, 'J': 0, '*': 0}
+
+    assert nombre_elements(['A', 'C', 'V'], AMINO_ACIDS) == {'A': 1, 'R': 0, 'N': 0, 'D': 0, 'C': 1, 'Q': 0,
+                                                              'E': 0, 'G': 0, 'H': 0, 'I': 0, 'L': 0, 'K': 0, 'M': 0,
+                                                              'F': 0, 'P': 0, 'O': 0, 'S': 0, 'U': 0, 'T': 0, 'W': 0,
+                                                              'Y': 0, 'V': 1, 'B': 0, 'Z': 0, 'X': 0, 'J': 0, '*': 0}
+
+    assert nombre_elements(['A', 'C', 'C', 'S', 'A', 'C', 'V'], AMINO_ACIDS) == {'A': 2, 'R': 0, 'N': 0, 'D': 0, 'C': 3, 'Q': 0,
+                                                                  'E': 0, 'G': 0, 'H': 0, 'I': 0, 'L': 0, 'K': 0, 'M': 0,
+                                                                  'F': 0, 'P': 0, 'O': 0, 'S': 1, 'U': 0, 'T': 0, 'W': 0,
+                                                                  'Y': 0, 'V': 1, 'B': 0, 'Z': 0, 'X': 0, 'J': 0, '*': 0}
