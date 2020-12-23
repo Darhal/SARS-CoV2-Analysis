e9998e335feb8281797e93982fe9efe4540f3e60
Chaima TOUNSI OMEZZINE <chaima.tounsi-omezzine@telecomnancy.eu>
2020-12-23 20:52:22 +0100

    fonction qrat to stop

--- src/codon.py
+++ src/codon.py
@@ -23,6 +23,7 @@ def codons(ARNm):
     
     return ''.join(amino_acids)
 
+
 def codons_echantillon(liste):
     '''Function that return the amino acids sequences coded by the ARNm sequence list
 
@@ -39,3 +40,29 @@ def codons_echantillon(liste):
         sortie.append(traduction)
 
     return sortie
+
+
+def start_to_stop(ARNm):
+    """
+
+    """
+    length = len(ARNm)
+    l = []
+    i = 0
+
+    while i < length-2 :
+
+        if ARNm[i:i+3] == 'AUG':
+            j = i
+
+            while (ARNm[j:j+3] not in ['UAG', 'UAA', 'UAR']) or (j == length-3) :
+                j += 1
+
+            if ARNm[j:j+3] in ['UAG', 'UAA', 'UAR']:
+                l.append(ARNm[i : j-1])
+                i = j+3
+
+            if j == length-3 :
+                break
+
+    return l
