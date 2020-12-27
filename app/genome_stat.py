c8d05f1694068cf7d0da66582db49c02a023e2be
Céline Zhang <celine.zhang@telecomnancy.eu>
2020-12-27 21:20:08 +0100

    midification des applications

--- app/genome_stat.py
+++ app/genome_stat.py
@@ -1,18 +1,48 @@
 from src.stats import *
 from src.levenshtein import *
 
-arns = fasta_to_genome("genome/200_sequences.fasta")
-stats = perform_all_stats(arns, NUCLEOTIDES)
 
-print(f"Moyenne: {stats['moy']}")
-print(f"Mediane: {stats['med']}")
-print(f"ecart type: {stats['ecartt']}")
-print(f"Variance: {stats['var']}")
-print(f"Quart 1: {stats['quart1']}")
-print(f"Quart 3: {stats['quart3']}")
-print(f"Inter Quart 3: {stats['int_quart']}")
+arns = fasta_to_genome("genome/200_sequences.fasta")
+
+
+####################### TAILLE #######################
+stats_taille = perform_all_stats_taille(fasta_to_genome("./genome/200_sequences.fasta"))
+
+print(f"Moyenne: {stats_taille['moy']}")
+print(f"Mediane: {stats_taille['med']}")
+print(f"ecart type: {stats_taille['ecartt']}")
+print(f"Variance: {stats_taille['var']}")
+print(f"Quart 1: {stats_taille['quart1']}")
+print(f"Quart 3: {stats_taille['quart3']}")
+print(f"Inter Quart 3: {stats_taille['int_quart']}")
+
+
+####################### NUCLEOTIDES #######################
+stats_nucleo = perform_all_stats(arns, NUCLEOTIDES)
+
+print(f"Moyenne: {stats_nucleo['moy']}")
+print(f"Mediane: {stats_nucleo['med']}")
+print(f"ecart type: {stats_nucleo['ecartt']}")
+print(f"Variance: {stats_nucleo['var']}")
+print(f"Quart 1: {stats_nucleo['quart1']}")
+print(f"Quart 3: {stats_nucleo['quart3']}")
+print(f"Inter Quart 3: {stats_nucleo['int_quart']}")
+
+# Pour 10000 séquences
+# arns = fasta_to_genome("../genome/10000_sequences.fasta")
+# stats = perform_all_stats(arns, NUCLEOTIDES)
+# print(stats)
+#
+# print(f"Moyenne: {stats['moy']}")
+# print(f"Mediane: {stats['med']}")
+# print(f"ecart type: {stats['ecartt']}")
+# print(f"Variance: {stats['var']}")
+# print(f"Quart 1: {stats['quart1']}")
+# print(f"Quart 3: {stats['quart3']}")
+# print(f"Inter Quart 3: {stats['int_quart']}")
 
 ####################### ACIDES AMINES #######################
+# sans utiliser la fonction perform_all_stats
 acids = codons_echantillon(arns)
 moy = call_stat(moyenne, acids, AMINO_ACIDS)
 med = call_stat(mediane, acids, AMINO_ACIDS)
@@ -29,73 +59,6 @@ print(f"Variance: {var}")
 print(f"Quart 1: {quart1}")
 print(f"Quart 3: {quart3}")
 print(f"Inter Quart 3: {int_quart}")
-
-# arns = fasta_to_genome("../genome/10000_sequences.fasta")
-# stats = perform_all_stats(arns, NUCLEOTIDES)
-# print(stats)
-#
-# print(f"Moyenne: {stats['moy']}")
-# print(f"Mediane: {stats['med']}")
-# print(f"ecart type: {stats['ecartt']}")
-# print(f"Variance: {stats['var']}")
-# print(f"Quart 1: {stats['quart1']}")
-# print(f"Quart 3: {stats['quart3']}")
-# print(f"Inter Quart 3: {stats['int_quart']}")
-
-###################### ACIDES AMINES #######################
-acids = codons_echantillon(arns)
-moy = call_stat(moyenne, acids, AMINO_ACIDS)
-med = call_stat(mediane, acids, AMINO_ACIDS)
-ecartt = call_stat(ecart_type, acids, AMINO_ACIDS)
-var = call_stat(variance, acids, AMINO_ACIDS)
-quart1 = call_stat(quartile, acids, AMINO_ACIDS, 1)
-quart3 = call_stat(quartile, acids, AMINO_ACIDS, 3)
-int_quart = call_stat(intervalle_interquartile, acids, AMINO_ACIDS)
-
-print(f"Moyenne: {moy}")
-print(f"Mediane: {med}")
-print(f"ecart type: {ecartt}")
-print(f"Variance: {var}")
-print(f"Quart 1: {quart1}")
-print(f"Quart 3: {quart3}")
-print(f"Inter Quart 3: {int_quart}")
-
-
-# arns = fasta_to_genome("../genome/10000_sequences.fasta")
-# stats = perform_all_stats(arns, NUCLEOTIDES)
-# print(stats)
-#
-#
-# print(f"Moyenne: {stats['moy']}")
-# print(f"Mediane: {stats['med']}")
-# print(f"ecart type: {stats['ecartt']}")
-# print(f"Variance: {stats['var']}")
-# print(f"Quart 1: {stats['quart1']}")
-# print(f"Quart 3: {stats['quart3']}")
-# print(f"Inter Quart 3: {stats['int_quart']}")
-
-
-###################### ACIDES AMINES #######################
-acids = codons_echantillon(arns)
-moy = call_stat(moyenne, acids, AMINO_ACIDS)
-med = call_stat(mediane, acids, AMINO_ACIDS)
-ecartt = call_stat(ecart_type, acids, AMINO_ACIDS)
-var = call_stat(variance, acids, AMINO_ACIDS)
-quart1 = call_stat(quartile, acids, AMINO_ACIDS, 1)
-quart3 = call_stat(quartile, acids, AMINO_ACIDS, 3)
-int_quart = call_stat(intervalle_interquartile, acids, AMINO_ACIDS)
-
-print(f"Moyenne: {moy}")
-print(f"Mediane: {med}")
-print(f"ecart type: {ecartt}")
-print(f"Variance: {var}")
-print(f"Quart 1: {quart1}")
-print(f"Quart 3: {quart3}")
-print(f"Inter Quart 3: {int_quart}")
-
-
-print(perform_all_stats(fasta_to_genome("./genome/dix_minisequences.fasta"), NUCLEOTIDES))
-print(call_stat(moyenne, fasta_to_genome("./genome/dix_sequences.fasta"), NUCLEOTIDES))
 
 
 ###################### STATS TAILLE DU ARNm SARS-COV2 ######################
