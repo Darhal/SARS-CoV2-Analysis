a9dcde037fa6703229f54a4d9847105c0109c869
Omar <chidaomar46@gmail.com>
2021-01-03 03:47:58 +0100

    Introduced multi-threading for faster measurement of performance.

--- src/performance.py
+++ src/performance.py
@@ -6,6 +6,7 @@ from src.levenshtein import *
 import matplotlib.ticker as ticker
 import random
 import string
+from multiprocessing import Pool # we will be doing some multiprocessing as some performance tests are painfully slow
 
 STRINGS = 1
 NUMBERS = 1 << 1
@@ -44,75 +45,27 @@ def arg_generator(N=10, stride=1, type=STRINGS, variant_arg_pos=[0], static_args
     return args
 
 
-def func_performance(func, args_arr, sizes, figure=True, sort_by=0, tick_spacing=1):
-    performance = []
+def measure_single_call(func, args, sizes):
+    start = timer() 
+    func(*args)
+    end = timer()
+    if len(sizes) > 1:
+        argument_size = tuple([len(args[i]) for i in sizes])
+    else:
+        argument_size = len(args[0])
+    ms = (end-start) / 1000
+    return (argument_size, ms)
 
-    for args in args_arr:
-        start = timer() 
-        func(*args)
-        end = timer()
-        if len(sizes) > 1:
-            argument_size = tuple([len(args[i]) for i in sizes])
-        else:
-            argument_size = len(args[0])
-        ms = (end-start) / 1000
-        performance.append((argument_size, ms))
-    
-    performance.sort(key=lambda p: p[sort_by])
-    
-    if figure:
-        plt.style.use('seaborn-whitegrid')
-        fig, ax = plt.subplots()
-        ax.plot([ str(t[0]) for t in performance ], [ t[1] for t in performance ])
-        ax.set_xlabel('Argument (s) Size')                  # Add an x-label to the axes.
-        ax.set_ylabel('Time (ms)')                          # Add a y-label to the axes.
-        ax.set_title(f"'{func.__name__}' Performance")      # Add a title to the axes.
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
-        if len(args_arr) > 70:
-            plt.xticks(fontsize=9, rotation=90)
-        plt.show()
-    return performance
-
-def funcs_performance(funcs, args_arr, sizes, figure=True, tick_spacing=1, title="Function Performance Comparison"):
-    performance = [ [] for _ in range(len(funcs))]
-    for i in range(len(funcs)):
-        for args in args_arr:
-            start = timer() 
-            funcs[i](*args)
-            end = timer()
-            if len(sizes) > 1:
-                argument_size = tuple([len(args[i]) for i in sizes])
-            else:
-                argument_size = len(args[0])
-            ms = (end-start) / 1000
-            performance[i].append((argument_size, ms))
-    
-        performance[i].sort(key=lambda p: p[0])
-    
-    if figure:
-        plt.style.use('seaborn-whitegrid')
-        fig, ax = plt.subplots()
-
-        for i in range(len(funcs)):
-            ax.plot([ str(t[0]) for t in performance[i] ], [ t[1] for t in performance[i] ], label=f'{funcs[i].__name__} performance')
-
-        ax.set_xlabel('Argument (s) Size')                  # Add an x-label to the axes.
-        ax.set_ylabel('Time (ms)')                          # Add a y-label to the axes.
-        ax.set_title(title)                                 # Add a title to the axes.
-        ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
-        ax.legend()
-
-        if len(args_arr) > 70:
-            plt.xticks(fontsize=9, rotation=90)
-        plt.show()
-    return performance
 
 def plot_multi_graph(graphs, legends, tick_spacing=1, title="Function Performance Comparison"):
     plt.style.use('seaborn-whitegrid')
     fig, ax = plt.subplots()
     
-    for i in range(len(graphs)):
-        ax.plot([ str(t[0]) for t in graphs[i] ], [ t[1] for t in graphs[i] ], label=legends[i])
+    if isinstance(graphs, list) and isinstance(graphs[0], list):
+        for i in range(len(graphs)):
+            ax.plot([ str(t[0]) for t in graphs[i] ], [ t[1] for t in graphs[i] ], label=legends[i])
+    else:
+        ax.plot([ str(t[0]) for t in graphs ], [ t[1] for t in graphs ], label=legends[0])
     
     ax.set_xlabel('Argument (s) Size')                  # Add an x-label to the axes.
     ax.set_ylabel('Time (ms)')                          # Add a y-label to the axes.
@@ -121,3 +74,67 @@ def plot_multi_graph(graphs, legends, tick_spacing=1, title="Function Performanc
     ax.legend()
     plt.xticks(fontsize=9, rotation=90)
     plt.show()
+
+
+def func_performance(func, args_arr, sizes, figure=True, sort_by=0, tick_spacing=1):
+    performance = []
+
+    for args in args_arr:
+        performance.append(measure_single_call(func, args, sizes))
+    
+    performance.sort(key=lambda p: p[sort_by])
+    
+    if figure:
+        plot_multi_graph(performance, legends=[f'{func.__name__} performance'], tick_spacing=tick_spacing, title=f'{func.__name__} performance')
+    return performance
+
+
+def funcs_performance(funcs, args_arr, sizes, figure=True, tick_spacing=1, title="Function Performance Comparison"):
+    performance = [ [] for _ in range(len(funcs))]
+
+    for i in range(len(funcs)):
+        for args in args_arr:
+            performance[i].append(measure_single_call(funcs[i], args, sizes))
+        
+        performance[i].sort(key=lambda p: p[0])
+    
+    if figure:
+        plot_multi_graph(performance, legends=[f'{func.__name__} performance' for func in funcs], tick_spacing=tick_spacing, title=title)
+    return performance
+
+
+# Multi-threading solutions
+
+def func_performance_mt(func, args_arr, sizes, figure=True, sort_by=0, tick_spacing=1):
+    performance = []
+    mt_pool = Pool()
+    performance = mt_pool.starmap(measure_single_call, [(func, args, sizes) for args in args_arr])
+    performance.sort(key=lambda p: p[sort_by])
+
+    if figure:
+        plot_multi_graph(performance, legends=[f'{func.__name__} performance'], tick_spacing=tick_spacing, title=f'{func.__name__} performance')
+    
+    return performance
+
+def funcs_performance_mt(funcs, args_arr, sizes, figure=True, tick_spacing=1, title="Function Performance Comparison"):
+    performance = []
+    mt_pool = Pool()
+    performance = mt_pool.starmap(func_performance, [(func, args_arr, sizes, False) for func in funcs])
+
+    if figure:
+        plot_multi_graph(performance, legends=[f'{func.__name__} performance' for func in funcs], tick_spacing=tick_spacing, title=title)
+    
+    return performance
+
+
+def funcs_performance_mt_v2(funcs, args_arr, sizes, figure=True, tick_spacing=1, title="Function Performance Comparison"):
+    performance = []
+
+    for func in funcs:
+        performance.append(func_performance_mt(func, args_arr, sizes, figure=False))
+        print(f"{func.__name__} finished.")
+
+    if figure:
+        plot_multi_graph(performance, legends=[f'{func.__name__} performance' for func in funcs], tick_spacing=tick_spacing, title=title)
+    
+    return performance
